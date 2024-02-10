c735df7eaee0cd120e6a6955d6765c6d579a9031
mohd jami <your_email@example.com>
2024-02-10 17:28:59 +0530

    Fixed changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,13 +9,23 @@ import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import HeaderRoutes from "./header-routes";
 
 const Navbar = async () => {
+  const session = await getServerSession(authOptions);
   return (
     <header className="sm:flex sm:justify-between   px-4  bg-slate-900 dark:bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0">
       <div className="relative px-4 sm:px-6 lg:px-8 flex h-12 items-center justify-between w-full mx-auto max-w-7xl">
         <HeaderRoutes />
         <div className="flex items-center justify-center">
           <ModeToggle /> &nbsp; &nbsp;
-          <UserAccountNav />
+          {session?.user ? (
+            <UserAccountNav />
+          ) : (
+            <Link
+              className={buttonVariants({ variant: "secondary" })}
+              href="/sign-in"
+            >
+              Sign in
+            </Link>
+          )}
         </div>
       </div>
     </header>
